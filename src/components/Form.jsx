f87df4cba766d8c0f35d3304b7e7db1815f532c5
Aquila Jedidiah <jedaqsaul@gmail.com>
2025-04-12 11:31:52 +0300

    Add the Forms.jsx files with props

---
+++ src/components/Form.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+function Form({ formData, handleChange, handleSubmit }) {
+  return (
+    <form onSubmit={handleSubmit} className="form">
+      <h1>Enter student Info</h1>
+      <input
+        type="text"
+        name="name"
+        placeholder="Enter Name"
+        value={formData.name}
+        onChange={handleChange}
+      />
+      <input
+        type="email"
+        name="email"
+        placeholder="Enter Email"
+        value={formData.email}
+        onChange={handleChange}
+      />
+      <input
+        type="tel"
+        name="phone"
+        placeholder="Enter Phone"
+        value={formData.phone}
+        onChange={handleChange}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default Form;
